2608d66dc4eef1eb3cb7863ab072145581437551
liam <liam@5d63ec18-c9f2-0310-919c-9735844cedf9>
2005-07-12 19:21:52 +0000

    Updates.
    git-svn-id: file:///home/svn/dot@35 5d63ec18-c9f2-0310-919c-9735844cedf9

--- wiki/t/sql/base-structure.sql
+++ wiki/t/sql/base-structure.sql
@@ -1,33 +1,22 @@
 -- 
--- Project  Name: None
--- File / Folder: base-structure.sql
+-- Project  Name: KB
+-- File / Folder: t/sql/base-structure.sql
 -- File Language: sql
 -- Copyright (C): 2005 Richard Group, Inc.
 -- First  Author: Liam Bryan
 -- First Created: 2005.06.21 12:36:17
 -- Last Modifier: Liam Bryan
--- Last Modified: 2005.06.21 15:43:41
+-- Last Modified: 2005.07.12 15:21:34
+--
 
-CREATE TABLE user (
-	user_id SERIAL PRIMARY KEY,
-	username VARCHAR(32) UNIQUE NOT NULL,
-	real_name VARCHAR(128),
-	password VARCHAR(32),
-	email VARCHAR(128) UNIQUE NOT NULL
-);
+\c kb
+\c - kb
 
 CREATE TABLE page (
-	page_id SERIAL PRIMARY KEY,
-	page_name VARCHAR(128),
-
-
-);
-
-CREATE TABLE revision (
-	page_id INTEGER REFERENCES page(page_id) NOT NULL,
-	revision_id SERIAL,
+	title VARCHAR(128),
+	revision_id INTEGER NOT NULL,
 	"text" TEXT,
+	change_log VARCHAR(256),
 	changed_on TIMESTAMP WITH TIME ZONE,
-	user_id INTEGER REFERENCES user(user_id) NOT NULL,
-	PRIMARY KEY(page_id, revision_id)
+	PRIMARY KEY(title, revision_id)
 );
