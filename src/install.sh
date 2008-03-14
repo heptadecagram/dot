97af3764b81fadae79f27f0e74a2e7ccee56cfa9
liam <liam@5d63ec18-c9f2-0310-919c-9735844cedf9>
2008-03-14 16:42:42 +0000

    Corrected server
    git-svn-id: file:///home/svn/dot@198 5d63ec18-c9f2-0310-919c-9735844cedf9

--- src/install.sh
+++ src/install.sh
@@ -5,10 +5,10 @@
 # Copyright (C): 2006 Liam Bryan
 # First  Author: Liam Bryan
 # First Created: 2006.07.18 13:27:11
-# Last Modifier: Liam Bryan
-# Last Modified: 2006.07.19 13:55:03
+# Last Modifier: Liam Echlin
+# Last Modified: 2008.02.29 03:17:47
 
-svn checkout svn+ssh://wa.heptadecagram.net/home/svn/dot $HOME/dot
+svn checkout svn+ssh://home.liamtara.net/home/svn/dot $HOME/dot
 mv $HOME/.ssh $HOME/.old.ssh
 mv $HOME/.subversion $HOME/.old.subversion
 mv $HOME/dot/.[a-z]* $HOME/
