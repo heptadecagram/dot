6a74cc5eb9ada9eb04b9a33d7197567558384b6a
Liam Echlin <wechlin@vetstreet.com>
2014-06-02 16:10:46 -0400

    Formatted more prettily

--- src/size.c
+++ src/size.c
@@ -6,7 +6,7 @@
  * First  Author: Liam Bryan
  * First Created: 2005.04.21 14:11:12
  * Last Modifier: Liam Echlin
- * Last Modified: 2010.11.30
+ * Last Modified: 2014.04.30
 
 */
 
@@ -15,23 +15,19 @@
 
 int main(int argc, char** argv) {
 	printf("size\ttype\n");
-	printf("%zu\t", sizeof(char) );
-	printf("char\n");
-	printf("%zu\t", sizeof(short) );
-	printf("short\n");
-	printf("%zu\t", sizeof(int) );
-	printf("int\n");
-	printf("%zu\t", sizeof(long) );
-	printf("long\n");
-	printf("%zu\t", sizeof(long int) );
-	printf("long int\n");
-	printf("%zu\t", sizeof(long long) );
-	printf("long long\n");
-	printf("%zu\t", sizeof(float) );
-	printf("float\n");
-	printf("%zu\t", sizeof(double) );
-	printf("double\n");
-	printf("%zu\t", sizeof(long double) );
-	printf("long double\n");
+	printf("%zu\tchar\n", sizeof(char) );
+	printf("%zu\tshort\n", sizeof(short) );
+	printf("%zu\tint\n", sizeof(int) );
+	printf("%zu\tlong\n", sizeof(long) );
+	printf("%zu\tlong int\n", sizeof(long int) );
+	printf("%zu\tlong long\n", sizeof(long long) );
+	printf("%zu\tfloat\n", sizeof(float) );
+	printf("%zu\tdouble\n", sizeof(double) );
+	printf("%zu\tlong double\n", sizeof(long double) );
+	printf("%zu\tsize_t\n", sizeof(size_t) );
+	printf("%zu\tchar*\n", sizeof(char*) );
+	printf("%zu\tlong double*\n", sizeof(long double*) );
+	printf("%zu\tvoid*\n", sizeof(void*) );
+	printf("%zu\tfunc*\n", sizeof(int(*)(int, int)));
 	return EXIT_SUCCESS;
 }
