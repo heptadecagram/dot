b3a51ccf9c42780cecde2e7246283837b0ad64d3
Liam Echlin <wechlin@umbctraining.com>
2015-03-03 09:57:14 -0500

    Headers must go!"

--- src/size.c
+++ src/size.c
@@ -1,14 +1,3 @@
-/*
- * Project  Name: None
- * File / Folder: size.c
- * File Language: c
- * Copyright (C): 2005 Liam Bryan
- * First  Author: Liam Bryan
- * First Created: 2005.04.21 14:11:12
- * Last Modifier: Liam Echlin
- * Last Modified: 2014.04.30
-
-*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,9 +14,11 @@ int main(int argc, char** argv) {
 	printf("%zu\tdouble\n", sizeof(double) );
 	printf("%zu\tlong double\n", sizeof(long double) );
 	printf("%zu\tsize_t\n", sizeof(size_t) );
-	printf("%zu\tchar*\n", sizeof(char*) );
-	printf("%zu\tlong double*\n", sizeof(long double*) );
 	printf("%zu\tvoid*\n", sizeof(void*) );
+	printf("%zu\tchar*\n", sizeof(char*) );
+	printf("%zu\tint*\n", sizeof(int*) );
+	printf("%zu\tlong double*\n", sizeof(long double*) );
 	printf("%zu\tfunc*\n", sizeof(int(*)(int, int)));
+
 	return EXIT_SUCCESS;
 }
