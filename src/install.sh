85944ad9fe987ea7f9048ade455ac24ee2dd710a
Liam Echlin <wechlin@umbctraining.com>
2015-04-26 12:31:15 -0400

    Headers are useless

--- src/install.sh
+++ src/install.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
-# Project  Name: None
-# File / Folder: install.sh
-# File Language: sh
-# Copyright (C): 2006 Liam Bryan
-# First  Author: Liam Bryan
-# First Created: 2006.07.18 13:27:11
-# Last Modifier: Liam Echlin
-# Last Modified: 2013.02.18
 
 git clone --origin=github https://github.com/heptadecagram/dot -- $HOME/dot
 mv $HOME/.ssh $HOME/.old.ssh
