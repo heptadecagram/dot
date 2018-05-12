f58e216299319183ecbe7fd0f4eeb844de78b2e2
Liam Echlin <wechlin@umbctraining.com>
2018-05-12 08:05:48 -0400

    Add endian test

--- src/size.c
+++ src/size.c
@@ -1,14 +1,32 @@
 
+#include <stdint.h>
 #include <stdio.h>
 
+union end_test {
+	uint16_t num;
+	struct {
+		uint8_t one;
+		uint8_t two;
+	};
+};
+
 int main(void) {
-	printf("size\ttype\n");
+	union end_test alfa;
+	alfa.num = 0x0102;
+	if(alfa.one == 0x01 && alfa.two == 0x02) {
+		puts("Big-Endian");
+	} else if(alfa.one == 0x02 && alfa.two == 0x01) {
+		puts("Little-Endian");
+	} else {
+		puts("Endianess unknown");
+	}
+	puts("size\ttype");
 	// Signed and unsigned are guaranteed to be the same size
 	printf("%zu\tchar\n", sizeof(char));
 	printf("%zu\tshort\n", sizeof(short));
 	printf("%zu\tint\n", sizeof(int));
-	printf("%zu\tlong int\n", sizeof(long int));
-	printf("%zu\tlong long\n", sizeof(long long int));
+	printf("%zu\tlong\n", sizeof(long));
+	printf("%zu\tlong long\n", sizeof(long long));
 
 	// _Complex types are guaranteed to be double their prefix
 	printf("%zu\tfloat\n", sizeof(float));
@@ -23,11 +41,9 @@ int main(void) {
 	printf("%zu\tvoid*\n", sizeof(void*));
 	printf("%zu\tchar*\n", sizeof(char*));
 	printf("%zu\tint*\n", sizeof(int*));
-	printf("%zu\tlong long int*\n", sizeof(long long int*));
+	printf("%zu\tlong long*\n", sizeof(long long*));
 	printf("%zu\tfloat*\n", sizeof(float*));
 	printf("%zu\tlong double*\n", sizeof(long double*));
 	// Function pointers have conversion guaranteed
 	printf("%zu\tfunc*\n", sizeof(void(*)()));
-
-	return 0;
 }
