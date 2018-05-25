8c1e256f1e401a47bcfc353cbbf91ae77abb311e
Liam Echlin <wechlin@umbctraining.com>
2018-05-25 17:41:41 -0400

    Update installation script

--- src/install.sh
+++ src/install.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-git clone --origin=github https://github.com/heptadecagram/dot -- $HOME/dot
-mv $HOME/.ssh $HOME/.old.ssh
+git clone --origin=github https://github.com/heptadecagram/dot.git -- $HOME/dot
+mv $HOME/dot/.local/* $HOME/.local/
+rmdir $HOME/dot/.local
+mv $HOME/dot/.ssh $HOME/.old.ssh
 mv $HOME/dot/.[a-z]* $HOME/
 mv $HOME/dot/* $HOME/
 rmdir $HOME/dot/
-if [ `expr $SHELL : bash` -eq 0 ]; then
+if [ `expr $SHELL : '.*bash'` -eq 0 ]; then
 	echo "Changing shell to `grep '/bash' /etc/shells` from $SHELL"
 	chsh -s `grep '/bash' /etc/shells` && exec `grep '/bash' /etc/shells` -l
 fi
