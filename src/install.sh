c610080d7d08241e3c7596202ca4e6a573fbe386
Liam Echlin <wechlin@umbctraining.com>
2018-12-19 11:38:05 -0500

    Fix install script to be more robust

--- src/install.sh
+++ src/install.sh
@@ -3,7 +3,7 @@
 git clone --origin=github https://github.com/heptadecagram/dot.git -- $HOME/dot
 mv $HOME/dot/.local/* $HOME/.local/
 rmdir $HOME/dot/.local
-mv $HOME/dot/.ssh $HOME/.old.ssh
+mv -n $HOME/dot/.ssh $HOME/.old.ssh
 mv $HOME/dot/.[a-z]* $HOME/
 mv $HOME/dot/* $HOME/
 rmdir $HOME/dot/
