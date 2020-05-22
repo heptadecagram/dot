79e024b63711bf4931f3b95c2e0437c0be0b97b3
Liam Echlin <wechlin@umbctraining.com>
2020-05-22 13:24:49 -0400

    Add player

--- src/c/wind/wind.c
+++ src/c/wind/wind.c
@@ -3,6 +3,30 @@
 
 #include "log.h"
 
+void write_room(int y, int x, int width, int height)
+{
+	mvaddwstr(y, x, L"┌");
+	for (int n=1; n < width-1; ++n) {
+		addwstr(L"─");
+	}
+	addwstr(L"┐");
+
+	for (int n=1; n < height-1; ++n) {
+		mvaddwstr(y+n, x, L"│");
+		mvaddwstr(y+n, x+width-1, L"│");
+	}
+
+	mvaddwstr(y+height-1, x, L"└");
+	for (int n=1; n < width-1; ++n) {
+		addwstr(L"─");
+	}
+	addwstr(L"┘");
+}
+
+struct coord {
+	int y, x;
+};
+
 int main(void)
 {
 	setlocale(LC_ALL, "");
@@ -14,12 +38,34 @@ int main(void)
 	intrflush(stdscr, false);
 	keypad(stdscr, true);
 
-	clear();
-	mvaddwstr(3, 4, L"┌────┐");
-	mvaddwstr(4, 4, L"│    │");
-	mvaddwstr(5, 4, L"└────┘");
-	refresh();
-	getch();
+	struct coord player = { 15, 15 };
+	int input = '\0';
+
+	while (input != 'q') {
+		clear();
+		write_room(2, 4, 5, 10);
+
+		write_room(15, 20, 2, 2);
+
+		mvaddch(player.y, player.x, '@');
+		move(player.y, player.x);
+		refresh();
+		input = getch();
+		switch (input) {
+			case KEY_UP:
+				player.y--;
+				break;
+			case KEY_DOWN:
+				player.y++;
+				break;
+			case KEY_LEFT:
+				player.x--;
+				break;
+			case KEY_RIGHT:
+				player.x++;
+				break;
+		}
+	}
 
 	endwin();
 }
