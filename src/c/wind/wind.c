ac23de8aba1cd83129d7fcef40a1aa4eae59b77c
Liam Echlin <wechlin@umbctraining.com>
2020-05-22 12:24:24 -0400

    Add simple curses display

--- src/c/wind/wind.c
+++ src/c/wind/wind.c
@@ -1,6 +1,25 @@
+#include <ncurses.h>
+#include <locale.h>
 
 #include "log.h"
 
 int main(void)
 {
+	setlocale(LC_ALL, "");
+	initscr();
+	cbreak();
+	noecho();
+
+	nonl();
+	intrflush(stdscr, false);
+	keypad(stdscr, true);
+
+	clear();
+	mvaddwstr(3, 4, L"┌────┐");
+	mvaddwstr(4, 4, L"│    │");
+	mvaddwstr(5, 4, L"└────┘");
+	refresh();
+	getch();
+
+	endwin();
 }
