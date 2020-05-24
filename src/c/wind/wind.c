56ad2ac621ee7dba21f806921c5a62e96d894925
Liam Echlin <wechlin@umbctraining.com>
2020-05-24 13:21:33 -0400

    Start building list of line-drawing characters

--- src/c/wind/wind.c
+++ src/c/wind/wind.c
@@ -80,7 +80,44 @@ void write_map(void)
 
 // XXX To make a wide character understandable to curses, build a cchar_t structure.
 // Second element should be an array of ints that make up the code point.
-cchar_t wa = { WA_BOLD, {'.'}, A_NORMAL};
+// attr, chars, ext_color
+cchar_t glyphs[] = {
+	{WA_NORMAL, {0x2500}, 0}, // ─ WACS_HLINE
+	{WA_NORMAL, {0x2501}, 0}, // ━
+	{WA_NORMAL, {0x2502}, 0}, // │ WACS_VLINE
+	{WA_NORMAL, {0x2503}, 0}, // ┃
+	{WA_NORMAL, {0x2504}, 0}, // ┄
+	{WA_NORMAL, {0x2505}, 0}, // ┅
+	{WA_NORMAL, {0x2506}, 0}, // ┆
+	{WA_NORMAL, {0x2507}, 0}, // ┇
+	{WA_NORMAL, {0x2508}, 0}, // ┈
+	{WA_NORMAL, {0x2509}, 0}, // ┉
+	{WA_NORMAL, {0x250a}, 0}, // ┊
+	{WA_NORMAL, {0x250b}, 0}, // ┋
+	{WA_NORMAL, {0x250c}, 0}, // ┌
+	{WA_NORMAL, {0x250d}, 0}, // ┍
+	{WA_NORMAL, {0x250e}, 0}, // ┎
+	{WA_NORMAL, {0x250f}, 0}, // ┏
+	{WA_NORMAL, {0x2510}, 0}, // ┐
+	{WA_NORMAL, {0x2511}, 0}, // ┑
+	{WA_NORMAL, {0x2512}, 0}, // ┒
+	{WA_NORMAL, {0x2513}, 0}, // ┓
+	{WA_NORMAL, {0x2514}, 0}, // └
+	{WA_NORMAL, {0x2515}, 0}, // ┕
+	{WA_NORMAL, {0x2516}, 0}, // ┖
+	{WA_NORMAL, {0x2517}, 0}, // ┗
+	{WA_NORMAL, {0x2518}, 0}, // ┘
+	{WA_NORMAL, {0x2519}, 0}, // ┙
+	{WA_NORMAL, {0x251a}, 0}, // ┚
+	{WA_NORMAL, {0x251b}, 0}, // ┛
+	{WA_NORMAL, {0x251c}, 0}, // ├
+	{WA_NORMAL, {0x251d}, 0}, // ┝
+	{WA_NORMAL, {0x251e}, 0}, // ┞
+	{WA_NORMAL, {0x251f}, 0}, // ┟
+	{WA_NORMAL, {0x2520}, 0}, // ┠
+	{WA_NORMAL, {0x2521}, 0}, // ┡
+	{WA_NORMAL, {0x256C}, 0}  // ╬
+};
 
 int main(int argc, char *argv[])
 {
@@ -122,12 +159,12 @@ int main(int argc, char *argv[])
 		if (map.glyphs[player.y][player.x]) {
 			add_wch(map.glyphs[player.y][player.x]);
 		} else {
-			add_wch(&wa);
+			add_wch(glyphs + 2);
 		}
 
 		switch (input) {
 			case 'b':
-				wa.attr ^= WA_BOLD;
+				glyphs[2].chars[0] = 0x2550;
 				break;
 			case KEY_UP:
 				if (player.y > 0 && !map.glyphs[player.y-1][player.x]) {
